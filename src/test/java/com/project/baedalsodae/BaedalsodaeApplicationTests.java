e0a80f872b993fad57e65e0f9053705e807ed2d1
byeongdoohan <quden04@gmail.com>
2026-02-24 21:57:01 +0900

    init project

---
+++ src/test/java/com/project/baedalsodae/BaedalsodaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.baedalsodae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaedalsodaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
