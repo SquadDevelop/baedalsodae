34d41d829c26209cb870890edcd4bb49eed3edf6
annieha <hajh5447@naver.com>
2026-02-26 13:03:02 +0900

    add: ManagerController 생성

---
+++ src/main/java/com/project/baedalsodae/admin/controller/ManagerController.java
@@ -0,0 +1,11 @@
+package com.project.baedalsodae.admin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/managers")
+@RequiredArgsConstructor
+public class ManagerController {
+}
