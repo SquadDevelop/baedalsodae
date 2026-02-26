9ea0045d9132f57505d59a2624687b33d2aef407
하물 <108536712+AnnieHa1002@users.noreply.github.com>
2026-02-26 16:29:55 +0900

    feat: #8 create base entity (#9)
    * feat: BaseTimeEntity 생성
    
    * feat: BaseAuditEntity 생성

---
+++ src/main/java/com/project/baedalsodae/global/common/entity/BaseAuditEntity.java
@@ -0,0 +1,47 @@
+package com.project.baedalsodae.global.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseAuditEntity extends BaseTimeEntity {
+
+  @CreatedBy
+  @Column(name = "created_by", updatable = false)
+  private Long createdBy;
+
+  @LastModifiedBy
+  @Column(name = "updated_by")
+  private Long updatedBy;
+
+  @Column(name = "deleted_at")
+  private LocalDateTime deletedAt;
+
+  @Column(name = "deleted_by")
+  private Long deletedBy;
+
+  @Column(name = "is_deleted")
+  @ColumnDefault("false")
+  private boolean isDeleted;
+
+  public void softDelete(Long userId) {
+    this.isDeleted = true;
+    this.deletedAt = LocalDateTime.now();
+    this.deletedBy = userId;
+  }
+
+  public void restore() {
+    this.isDeleted = false;
+    this.deletedAt = null;
+    this.deletedBy = null;
+  }
+}
