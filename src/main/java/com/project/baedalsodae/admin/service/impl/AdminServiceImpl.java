0c6fab48f267f5f911d59677af95de2af508f72d
annieha <hajh5447@naver.com>
2026-02-26 13:52:38 +0900

    fix: AdminServiceImpl package 이름 변경

---
+++ src/main/java/com/project/baedalsodae/admin/service/impl/AdminServiceImpl.java
@@ -0,0 +1,9 @@
+package com.project.baedalsodae.admin.service.impl;
+
+import com.project.baedalsodae.admin.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminServiceImpl implements AdminService {}
--- src/main/java/com/project/baedalsodae/admin/service/implement/AdminServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.project.baedalsodae.admin.service.implement;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class AdminServiceImpl {}
