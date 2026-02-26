28e7f17b30072998fde1cbdc1c0688f9ebaa91dd
annieha <hajh5447@naver.com>
2026-02-26 13:51:53 +0900

    fix: @Controller to @RestController

--- src/main/java/com/project/baedalsodae/admin/controller/AdminController.java
+++ src/main/java/com/project/baedalsodae/admin/controller/AdminController.java
@@ -1,10 +1,10 @@
 package com.project.baedalsodae.admin.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/admins")
 @RequiredArgsConstructor
 public class AdminController {
