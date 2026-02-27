dc29e383d41e1a0b9c9adab53273cc5a4bd02d9d
annieha <hajh5447@naver.com>
2026-02-27 23:29:12 +0900

    feat: MenuCategory엔티티 생성

---
+++ src/main/java/com/project/baedalsodae/menu/entity/MenuCategory.java
@@ -0,0 +1,27 @@
+package com.project.baedalsodae.menu.entity;
+
+import com.project.baedalsodae.global.common.entity.BaseAuditEntity;
+import jakarta.persistence.*;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "p_menu_category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MenuCategory extends BaseAuditEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "order_no", nullable = false)
+    private int orderNo;
+
+}
