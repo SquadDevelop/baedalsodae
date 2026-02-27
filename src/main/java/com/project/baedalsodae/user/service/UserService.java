49868b32e552c90f9810570198182d13321e10fa
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-02-28 01:19:26 +0900

    feat: 회원 Service 추가

---
+++ src/main/java/com/project/baedalsodae/user/service/UserService.java
@@ -0,0 +1,4 @@
+package com.project.baedalsodae.user.service;
+
+public interface UserService {
+}
---
+++ src/main/java/com/project/baedalsodae/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.project.baedalsodae.user.service.impl;
+
+import com.project.baedalsodae.user.service.UserService;
+
+public class UserServiceImpl implements UserService {
+}
