ff4145248cafdf4cee4c523150e38ba25359056f
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-02-28 01:16:29 +0900

    feat: 회원 도메인 엔티티 생성

---
+++ src/main/java/com/project/baedalsodae/user/entity/User.java
@@ -0,0 +1,51 @@
+package com.project.baedalsodae.user.entity;
+
+import com.project.baedalsodae.global.common.entity.BaseAuditEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "p_user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseAuditEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "username", nullable = false, length = 20, unique = true)
+    private String username;
+
+    @Column(name = "phone", nullable = false, length = 100)
+    private String phone;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "nickname", nullable = false, length = 100, unique = true)
+    private String nickname;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Column(name = "user_main_address_id")
+    private Long userMainAddressId;
+}
---
+++ src/main/java/com/project/baedalsodae/user/entity/UserAddress.java
@@ -0,0 +1,42 @@
+package com.project.baedalsodae.user.entity;
+
+import com.project.baedalsodae.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "p_user_address")
+public class UserAddress extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "sido_id", nullable = false)
+    private UUID sidoId;
+
+    @Column(name = "sigungu_id", nullable = false)
+    private UUID sigunguId;
+
+    @Column(name = "dong_id", nullable = false)
+    private UUID dongId;
+
+    @Column(name = "road_address", nullable = false)
+    private String roadAddress;
+
+    @Column(name = "detail_address", nullable = false)
+    private String detailAddress;
+
+    @Column(name = "description")
+    private String description;
+}
