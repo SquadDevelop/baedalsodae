a062c6f530c992ee5b06931efba20f76dcff1754
annieha <hajh5447@naver.com>
2026-02-28 00:15:06 +0900

    feat: tagMapping entity 생성

---
+++ src/main/java/com/project/baedalsodae/tag/entity/TagMapping.java
@@ -0,0 +1,32 @@
+package com.project.baedalsodae.tag.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_tag")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TagMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id", nullable = false)
+    private Tag tag;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}
