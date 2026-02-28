6092231b1ad7ff2d353bbcdda2db487b88db95bd
jaebin1234 <jaebin2586@gmail.com>
2026-02-28 16:55:44 +0900

    feat: 장바구니 Controller 초기 세팅

---
+++ src/main/java/com/project/baedalsodae/cart/controller/CartController.java
@@ -0,0 +1,16 @@
+package com.project.baedalsodae.cart.controller;
+
+import com.project.baedalsodae.cart.service.CartService;
+import com.project.baedalsodae.global.common.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/cart")
+@RequiredArgsConstructor
+public class CartController {
+
+}
