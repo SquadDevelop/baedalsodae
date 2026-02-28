112cecc11fe51503571bd4b7e1148e400503f95d
Hyeonbin2379 <hyeonbin2379@gmail.com>
2026-02-28 14:04:42 +0900

    Feat/10 security setup (#22)
    * feat: 회원 도메인 엔티티 생성
    
    * feat: 회원 Controller 추가
    
    * feat: 회원 Service 추가
    
    * fix: id 필드 Column 어노테이션 추가
    
    * feat: 스프링 시큐리티 관련 초기 세팅
    
    * feat: createdBy, modifiedBy Auditing 관련 초기 세팅
    
    * fix: 회원 주소 엔터티 NoArgsConstructor 추가
    
    * fix: UserServiceImpl Service 어노테이션 추가

---
+++ src/main/java/com/project/baedalsodae/user/entity/UserAddress.java
@@ -0,0 +1,46 @@
+package com.project.baedalsodae.user.entity;
+
+import com.project.baedalsodae.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "p_user_address")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserAddress extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "sido_id", nullable = false)
+    private UUID sidoId;
+
+    @Column(name = "sigungu_id", nullable = false)
+    private UUID sigunguId;
+
+    @Column(name = "dong_id", nullable = false)
+    private UUID dongId;
+
+    @Column(name = "road_address", nullable = false)
+    private String roadAddress;
+
+    @Column(name = "detail_address", nullable = false)
+    private String detailAddress;
+
+    @Column(name = "description")
+    private String description;
+}
---
+++ src/main/java/com/project/baedalsodae/user/entity/UserRole.java
@@ -0,0 +1,18 @@
+package com.project.baedalsodae.user.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+
+    CUSTOMER("ROLE_CUSTOMER"),
+    OWNER("ROLE_OWNER"),
+    MANAGER("ROLE_MANAGER"),
+    MASTER("ROLE_MASTER");
+
+    private final String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+}
