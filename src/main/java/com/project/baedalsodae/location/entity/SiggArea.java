9a85816f64262d60590ec5e9d68f489a5ef462fb
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-02-28 20:51:35 +0900

    fix: NoArgConstructor 추가

--- src/main/java/com/project/baedalsodae/location/entity/SiggArea.java
+++ src/main/java/com/project/baedalsodae/location/entity/SiggArea.java
@@ -8,11 +8,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.UUID;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @Table(name = "p_sigg_area")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SiggArea extends BaseTimeEntity {
 
     @Id
