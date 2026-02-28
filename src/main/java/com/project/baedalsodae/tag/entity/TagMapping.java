f7c784241b1a8ff51b0cae46157b8713efdb358f
하물 <108536712+AnnieHa1002@users.noreply.github.com>
2026-03-01 02:24:42 +0900

    fix: 테이블 이름 명칭 수정 (#38)
    p_tag -> p_tag_mapping

--- src/main/java/com/project/baedalsodae/tag/entity/TagMapping.java
+++ src/main/java/com/project/baedalsodae/tag/entity/TagMapping.java
@@ -2,41 +2,38 @@ package com.project.baedalsodae.tag.entity;
 
 import com.project.baedalsodae.menu.entity.MenuItem;
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 @Entity
-@Table(name = "p_tag")
+@Table(name = "p_tag_mapping")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
 public class TagMapping {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "id", nullable = false)
-    private UUID id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "id", nullable = false)
+  private UUID id;
 
-    @Column(name = "order_no", nullable = false)
-    private int orderNo;
+  @Column(name = "order_no", nullable = false)
+  private int orderNo;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "tag_id", nullable = false)
-    private Tag tag;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "tag_id", nullable = false)
+  private Tag tag;
 
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "menu_item", nullable = false)
+  private MenuItem menuItem;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "menu_item", nullable = false)
-    private MenuItem menuItem;
-
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
 }
