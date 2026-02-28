0025c39a55733027e5501865e1d1da616fe8940d
하물 <108536712+AnnieHa1002@users.noreply.github.com>
2026-02-28 17:40:27 +0900

    Feat/12 tag and tag mapping entity (#20)
    * feat: Tag entity 생성
    
    * feat: tagMapping entity 생성
    
    * fix: EntityListeners added
    
    * feat: MenuItem 연관관계 설정

---
+++ src/main/java/com/project/baedalsodae/tag/entity/Tag.java
@@ -0,0 +1,32 @@
+package com.project.baedalsodae.tag.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_tag")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}
