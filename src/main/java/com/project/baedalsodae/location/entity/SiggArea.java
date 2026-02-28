468bb465e2df45ae70505fcfba422a399483f23a
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-02-28 20:26:53 +0900

    feat: 지역 도메인 엔티티 추가

---
+++ src/main/java/com/project/baedalsodae/location/entity/SiggArea.java
@@ -0,0 +1,31 @@
+package com.project.baedalsodae.location.entity;
+
+import com.project.baedalsodae.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "p_sigg_area")
+public class SiggArea extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "adm_code", nullable = false, length = 50, unique = true)
+    private String admCode;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "version", length = 20)
+    private String version;
+}
