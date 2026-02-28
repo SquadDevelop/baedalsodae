b44647f82b2a9b4432f8c4f523dd11e315661eca
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-02-28 14:03:31 +0900

    fix: UserServiceImpl Service 어노테이션 추가

--- src/main/java/com/project/baedalsodae/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/project/baedalsodae/user/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.project.baedalsodae.user.service.impl;
 
 import com.project.baedalsodae.user.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 }
