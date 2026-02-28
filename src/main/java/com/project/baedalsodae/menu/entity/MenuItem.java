2e4eb6e93066f64da65f3d87816a8c47531031fc
annieha <hajh5447@naver.com>
2026-02-28 17:37:44 +0900

    feat: MenuItem 연관관계 설정

--- src/main/java/com/project/baedalsodae/menu/entity/MenuItem.java
+++ src/main/java/com/project/baedalsodae/menu/entity/MenuItem.java
@@ -2,12 +2,14 @@ package com.project.baedalsodae.menu.entity;
 
 import com.project.baedalsodae.global.common.entity.BaseAuditEntity;
 import com.project.baedalsodae.menu.entity.enums.MenuStatus;
+import com.project.baedalsodae.tag.entity.TagMapping;
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.UUID;
 
 @Getter
 @Entity
@@ -15,32 +17,34 @@ import java.util.UUID;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MenuItem extends BaseAuditEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "id", nullable = false)
-    private UUID id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "id", nullable = false)
+  private UUID id;
 
-    @Column(name = "is_popular", nullable = false)
-    private boolean isPopular =false;
+  @Column(name = "is_popular", nullable = false)
+  private boolean isPopular = false;
 
-    @Column(name = "name", nullable = false)
-    private String name;
+  @Column(name = "name", nullable = false)
+  private String name;
 
-    @Column(name = "order_no", nullable = false)
-    private int orderNo;
+  @Column(name = "order_no", nullable = false)
+  private int orderNo;
 
-    @Column(name = "price", nullable = false)
-    private int price;
+  @Column(name = "price", nullable = false)
+  private int price;
 
-    @Column(name = "description", columnDefinition = "TEXT")
-    private String description;
+  @Column(name = "description", columnDefinition = "TEXT")
+  private String description;
 
-    @JoinColumn(name = "menu_category_id", nullable = false)
-    @ManyToOne(fetch = FetchType.LAZY)
-    private MenuCategory menuCategory;
+  @JoinColumn(name = "menu_category_id", nullable = false)
+  @ManyToOne(fetch = FetchType.LAZY)
+  private MenuCategory menuCategory;
 
-    @Column(name = "menu_status")
-    @Enumerated(EnumType.STRING)
-    private MenuStatus menuStatus;
+  @OneToMany(mappedBy = "menuItem", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<TagMapping> tagMappings = new ArrayList<>();
 
+  @Column(name = "menu_status")
+  @Enumerated(EnumType.STRING)
+  private MenuStatus menuStatus;
 }
--- src/main/java/com/project/baedalsodae/tag/entity/TagMapping.java
+++ src/main/java/com/project/baedalsodae/tag/entity/TagMapping.java
@@ -1,5 +1,6 @@
 package com.project.baedalsodae.tag.entity;
 
+import com.project.baedalsodae.menu.entity.MenuItem;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -22,10 +23,17 @@ public class TagMapping {
     @Column(name = "id", nullable = false)
     private UUID id;
 
+    @Column(name = "order_no", nullable = false)
+    private int orderNo;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "tag_id", nullable = false)
     private Tag tag;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_item", nullable = false)
+    private MenuItem menuItem;
 
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
