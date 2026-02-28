e3bd99e8796921b855460444836b80acf1bcfa52
annieha <hajh5447@naver.com>
2026-02-28 10:00:44 +0900

    fix: EntityListeners added

--- src/main/java/com/project/baedalsodae/tag/entity/Tag.java
+++ src/main/java/com/project/baedalsodae/tag/entity/Tag.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Table(name = "p_tag")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Tag {
 
     @Id
