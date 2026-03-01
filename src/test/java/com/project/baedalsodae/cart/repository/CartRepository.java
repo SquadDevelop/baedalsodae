105cdf20ac9589d1b735892f29bddb4fe7ad75f0
jaebin1234 <jaebin2586@gmail.com>
2026-03-01 18:51:16 +0900

    feat: CartRepository 추가

---
+++ src/test/java/com/project/baedalsodae/cart/repository/CartRepository.java
@@ -0,0 +1,12 @@
+package com.project.baedalsodae.cart.repository;
+
+import com.project.baedalsodae.cart.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, UUID> {
+
+}
