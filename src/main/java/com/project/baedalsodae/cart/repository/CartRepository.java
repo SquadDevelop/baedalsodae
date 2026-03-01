5f8abb40665b04653b5ad5b37fdc1aaadca6080e
jaebin1234 <jaebin2586@gmail.com>
2026-03-01 22:56:31 +0900

    fix: 장바구니 조회 N+1 문제 해결 (fetch join 적용)

--- src/main/java/com/project/baedalsodae/cart/repository/CartRepository.java
+++ src/main/java/com/project/baedalsodae/cart/repository/CartRepository.java
@@ -2,6 +2,8 @@ package com.project.baedalsodae.cart.repository;
 
 import com.project.baedalsodae.cart.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,9 @@ import java.util.UUID;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, UUID> {
-	Optional<Cart> findByUserIdAndIsDeletedFalse(UUID userId);
+		@Query("select c from Cart c " +
+				"left join fetch c.items i " +
+				"left join fetch i.menuItem " +
+				"where c.userId = :userId and c.isDeleted = false")
+	Optional<Cart> findByUserIdAndIsDeletedFalse(@Param("userId") UUID userId);
 }
