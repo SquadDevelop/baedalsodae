ca8424b9726d564496dfe37077bdbb4c62426481
annieha <hajh5447@naver.com>
2026-03-02 21:28:55 +0900

    feat: MenuCategory, MenuItem 엔티티에 Orderable 적용

--- src/main/java/com/project/baedalsodae/menu/entity/MenuItem.java
+++ src/main/java/com/project/baedalsodae/menu/entity/MenuItem.java
@@ -1,6 +1,7 @@
 package com.project.baedalsodae.menu.entity;
 
 import com.project.baedalsodae.global.common.entity.BaseAuditEntity;
+import com.project.baedalsodae.menu.common.Orderable;
 import com.project.baedalsodae.menu.entity.enums.MenuStatus;
 import com.project.baedalsodae.tag.entity.TagMapping;
 import jakarta.persistence.*;
@@ -13,9 +14,15 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@Table(name = "p_menu_item")
+@Table(
+    name = "p_menu_item",
+    uniqueConstraints = {
+      @UniqueConstraint(
+          name = "uq_menu_item_order_no",
+          columnNames = {"menu_category_id", "order_no"})
+    })
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class MenuItem extends BaseAuditEntity {
+public class MenuItem extends BaseAuditEntity implements Orderable {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
@@ -79,15 +86,16 @@ public class MenuItem extends BaseAuditEntity {
     this.menuStatus = menuStatus;
   }
 
-  public void changeOrderNo(Integer integer) {
-    this.orderNo = integer;
-  }
-
   public void changeIsPopular(Boolean popular) {
     this.isPopular = popular;
   }
 
   public void changeMenuCategory(MenuCategory category) {
     this.menuCategory = category;
+  }
+
+  @Override
+  public void changeOrderNo(int orderNo) {
+    this.orderNo = orderNo;
   }
 }
