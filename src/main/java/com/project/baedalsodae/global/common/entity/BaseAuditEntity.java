56d6eba711e0c1990a29a8807f23e05238510421
hyew0nn <hyewon-0607@naver.com>
2026-03-02 20:03:11 +0900

    fix: 서버 시간 일관성을 위해 공통 엔티티 LocalDateTime 을 Instant 로 변경

--- src/main/java/com/project/baedalsodae/global/common/entity/BaseAuditEntity.java
+++ src/main/java/com/project/baedalsodae/global/common/entity/BaseAuditEntity.java
@@ -3,7 +3,8 @@ package com.project.baedalsodae.global.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+
+import java.time.Instant;
 import java.util.UUID;
 
 import lombok.Getter;
@@ -26,7 +27,7 @@ public class BaseAuditEntity extends BaseTimeEntity {
   private UUID updatedBy;
 
   @Column(name = "deleted_at")
-  private LocalDateTime deletedAt;
+  private Instant deletedAt;
 
   @Column(name = "deleted_by")
   private UUID deletedBy;
@@ -37,7 +38,7 @@ public class BaseAuditEntity extends BaseTimeEntity {
 
   public void softDelete(UUID userId) {
     this.isDeleted = true;
-    this.deletedAt = LocalDateTime.now();
+    this.deletedAt = Instant.now();
     this.deletedBy = userId;
   }
 
--- src/main/java/com/project/baedalsodae/global/common/entity/BaseTimeEntity.java
+++ src/main/java/com/project/baedalsodae/global/common/entity/BaseTimeEntity.java
@@ -3,7 +3,8 @@ package com.project.baedalsodae.global.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+
+import java.time.Instant;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -16,9 +17,9 @@ public class BaseTimeEntity {
 
   @CreatedDate
   @Column(name = "created_at", updatable = false)
-  private LocalDateTime createdAt;
+  private Instant createdAt;
 
   @LastModifiedDate
   @Column(name = "updated_at")
-  private LocalDateTime updatedAt;
+  private Instant updatedAt;
 }
