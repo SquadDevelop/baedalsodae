c44ec3de554afcac88da308eb138d0977638befe
annieha <hajh5447@naver.com>
2026-03-02 23:02:53 +0900

    fix: localdateTime 을 Instant로 변경

--- src/main/java/com/project/baedalsodae/global/common/entity/BaseAuditEntity.java
+++ src/main/java/com/project/baedalsodae/global/common/entity/BaseAuditEntity.java
@@ -3,6 +3,8 @@ package com.project.baedalsodae.global.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -26,7 +28,7 @@ public class BaseAuditEntity extends BaseTimeEntity {
   private UUID updatedBy;
 
   @Column(name = "deleted_at")
-  private LocalDateTime deletedAt;
+  private Instant deletedAt;
 
   @Column(name = "deleted_by")
   private UUID deletedBy;
@@ -37,7 +39,7 @@ public class BaseAuditEntity extends BaseTimeEntity {
 
   public void softDelete(UUID userId) {
     this.isDeleted = true;
-    this.deletedAt = LocalDateTime.now();
+    this.deletedAt = Instant.now();
     this.deletedBy = userId;
   }
 
