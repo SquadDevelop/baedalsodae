97cf1a544abc2bfe45766ed23cd7be42452bcc37
하물 <108536712+AnnieHa1002@users.noreply.github.com>
2026-03-04 21:23:27 +0900

    Feat/60 get tags (#61)
    * feat: 태그 검색 조회기능
    
    * test: 테스트 코드 추가

---
+++ src/main/java/com/project/baedalsodae/tag/dto/responseDto/TagResponseDto.java
@@ -0,0 +1,10 @@
+package com.project.baedalsodae.tag.dto.responseDto;
+
+import com.project.baedalsodae.tag.entity.Tag;
+import java.util.UUID;
+
+public record TagResponseDto(UUID id, String name) {
+  public static TagResponseDto fromEntity(Tag tag) {
+    return new TagResponseDto(tag.getId(), tag.getName());
+  }
+}
