23299362ffbc0e3602f3a50f0d99ea06a9eaac07
jaebin1234 <jaebin2586@gmail.com>
2026-03-04 15:46:34 +0900

    feat: 주문생성 SuccessCode 추가

--- src/main/java/com/project/baedalsodae/global/common/SuccessCode.java
+++ src/main/java/com/project/baedalsodae/global/common/SuccessCode.java
@@ -13,7 +13,11 @@ public enum SuccessCode {
 	CART_ITEM_ADDED("CT201", HttpStatus.OK, "장바구니 아이템 추가 성공"),
 	CART_ITEM_QUANTITY_UPDATED("CT202", HttpStatus.OK, "장바구니 아이템 수량 변경 성공"),
 	CART_ITEM_REMOVED("CT203", HttpStatus.OK, "장바구니 아이템 삭제 성공"),
-	CART_CLEARED("CT204", HttpStatus.OK, "장바구니 비우기 성공")
+	CART_CLEARED("CT204", HttpStatus.OK, "장바구니 비우기 성공"),
+
+	//order
+	ORDER_CREATED("OR201", HttpStatus.CREATED, "주문 생성 성공"),
+
 	;
 
 	private final String code;
