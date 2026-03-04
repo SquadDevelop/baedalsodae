1e608b2473637934fa291119b1bffc54cb8b7350
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-03-04 15:19:14 +0900

    feat: 회원주소 관련 에러코드 추가

--- src/main/java/com/project/baedalsodae/global/common/ErrorCode.java
+++ src/main/java/com/project/baedalsodae/global/common/ErrorCode.java
@@ -25,7 +25,8 @@ public enum ErrorCode {
     USER_DUPLICATED_USERNAME("U004", HttpStatus.CONFLICT, "이미 사용 중인 아이디입니다."),
     USER_DUPLICATED_NICKNAME("U005", HttpStatus.CONFLICT, "이미 사용 중인 닉네임입니다."),
     USER_ADDRESS_NOT_FOUND("U006", HttpStatus.NOT_FOUND, "해당 회원의 주소지를 찾을 수 없습니다."),
-    CANNOT_DELETE_ADDRESS("U007", HttpStatus.BAD_REQUEST, "회원 주소를 더 이상 삭제할 수 없습니다."),
+    USER_ADDRESS_CANNOT_DELETE("U007", HttpStatus.BAD_REQUEST, "회원 주소를 더 이상 삭제할 수 없습니다."),
+    USER_ADDRESS_DUPLICATED("U008", HttpStatus.BAD_REQUEST, "중복된 회원주소가 존재합니다."),
 
     MENU_CATEGORY_NOT_FOUND("MC001", HttpStatus.NOT_FOUND, "메뉴 카테고리가 없습니다"),
 
