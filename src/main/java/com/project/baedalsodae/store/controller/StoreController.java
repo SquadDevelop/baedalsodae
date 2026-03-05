8f4e47047b2c06373740083a612766cd851c8d56
annieha <hajh5447@naver.com>
2026-03-05 11:57:04 +0900

    feat: MenuCategory 생성 API 연결
    # Conflicts:
    #	src/main/java/com/project/baedalsodae/global/common/SuccessCode.java

--- src/main/java/com/project/baedalsodae/store/controller/StoreController.java
+++ src/main/java/com/project/baedalsodae/store/controller/StoreController.java
@@ -2,6 +2,9 @@ package com.project.baedalsodae.store.controller;
 
 import com.project.baedalsodae.global.common.ApiResponse;
 import com.project.baedalsodae.global.common.SuccessCode;
+import com.project.baedalsodae.menu.dto.requestDto.category.MenuCategoryPostRequestDto;
+import com.project.baedalsodae.menu.dto.responseDto.category.MenuCategoryResponseDto;
+import com.project.baedalsodae.menu.service.MenuCategoryService;
 import com.project.baedalsodae.store.dto.request.CreateStoreRequest;
 import com.project.baedalsodae.store.dto.request.UpdateStoreRequest;
 import com.project.baedalsodae.store.dto.request.UpdateStoreStatusRequest;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/stores")
 public class StoreController {
     private final StoreCommandService storeCommandService;
+    private final MenuCategoryService menuCategoryService;
 
     // TODO 인증 도메인 완료되면, userId 추가
     @PostMapping
@@ -50,5 +54,12 @@ public class StoreController {
             @RequestHeader("X-User-Id") UUID userId, @PathVariable UUID storeId) {
         storeCommandService.deleteStore(storeId, userId);
         return ResponseEntity.ok(ApiResponse.success(SuccessCode.STORE_DELETED, null));
+    }
+
+    @PostMapping("/{storeId}/menu-categories")
+    public ResponseEntity<ApiResponse<MenuCategoryResponseDto>> createMenuCategory(
+            @PathVariable UUID storeId, @RequestBody @Valid MenuCategoryPostRequestDto request) {
+        MenuCategoryResponseDto response = menuCategoryService.createMenuCategory(storeId, request);
+        return ResponseEntity.ok(ApiResponse.success(SuccessCode.MENU_CATEGORY_CREATED, response));
     }
 }
