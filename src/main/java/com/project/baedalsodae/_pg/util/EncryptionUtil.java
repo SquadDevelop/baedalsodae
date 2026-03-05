112739914cb5e0b864e2440fc930d02370c38043
byeongdoohan <quden04@gmail.com>
2026-03-05 12:23:27 +0900

    feat: encryption util 추가

---
+++ src/main/java/com/project/baedalsodae/_pg/util/EncryptionUtil.java
@@ -0,0 +1,54 @@
+package com.project.baedalsodae._pg.util;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EncryptionUtil {
+    private final Logger logger = LoggerFactory.getLogger(EncryptionUtil.class);
+    private SecretKeySpec secretKey;
+
+    @Value("${encryption.aes.key}")
+    public void setSecretKey(String myKey) {
+        try {
+            byte[] key = myKey.getBytes(StandardCharsets.UTF_8);
+            MessageDigest sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String encrypt(String strToEncrypt) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder()
+                    .encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
+        } catch (Exception e) {
+            logger.warn("[PG Encryption] Encryption failed for input: {}", strToEncrypt, e);
+        }
+        return null;
+    }
+
+    public String decrypt(String strToDecrypt) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        } catch (Exception e) {
+            logger.warn("[PG Decryption] Decryption failed for input: {}", strToDecrypt, e);
+        }
+        return null;
+    }
+}
