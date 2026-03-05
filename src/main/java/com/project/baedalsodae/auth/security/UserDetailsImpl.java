5d878431e60220f67d26abdf7d4d029d77ba997e
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-03-06 02:19:17 +0900

    feat: UserDetails, UserDetailsService 구현

---
+++ src/main/java/com/project/baedalsodae/auth/security/UserDetailsImpl.java
@@ -0,0 +1,89 @@
+package com.project.baedalsodae.auth.security;
+
+import com.project.baedalsodae.user.entity.User;
+import com.project.baedalsodae.user.entity.UserRole;
+import io.jsonwebtoken.Claims;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.UUID;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserDetailsImpl implements UserDetails {
+
+	private final String username;
+	private final String password;
+
+	@Getter
+	private final UUID userId;
+
+	@Getter
+	private final UserRole userRole;
+
+	@Getter
+	private final boolean isDeleted;
+
+	public static UserDetailsImpl from(User user) {
+		return UserDetailsImpl.builder()
+				.userId(user.getId())
+				.username(user.getUsername())
+				.password(user.getPassword())
+				.userRole(user.getRole())
+				.isDeleted(user.isDeleted())
+				.build();
+	}
+
+	public static UserDetailsImpl from(Claims claims) {
+		return UserDetailsImpl.builder()
+				.userId(UUID.fromString(claims.get("userId", String.class)))
+				.username(claims.getSubject())
+				.password(null)
+				.userRole(UserRole.of(claims.get("userRole", String.class)))
+				.isDeleted(claims.get("isDeleted", Boolean.class))
+				.build();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Collection<SimpleGrantedAuthority> grantedAuthority = new ArrayList<>();
+
+		grantedAuthority.add(new SimpleGrantedAuthority(this.userRole.getRole()));
+
+		return grantedAuthority;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return !isDeleted;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return !isDeleted;
+	}
+}
---
+++ src/main/java/com/project/baedalsodae/auth/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.project.baedalsodae.auth.service;
+
+import com.project.baedalsodae.auth.security.UserDetailsImpl;
+import com.project.baedalsodae.global.common.ErrorCode;
+import com.project.baedalsodae.user.entity.User;
+import com.project.baedalsodae.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException(ErrorCode.USER_NOT_FOUND.getMessage()));
+
+		return UserDetailsImpl.from(user);
+	}
+}
