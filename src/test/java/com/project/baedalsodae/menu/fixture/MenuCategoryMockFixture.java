ae7107f51de53f8301e5c868bb9f9fd698483425
annieha <hajh5447@naver.com>
2026-03-06 15:20:48 +0900

    fix: 코드 포멧

--- src/test/java/com/project/baedalsodae/menu/fixture/MenuCategoryMockFixture.java
+++ src/test/java/com/project/baedalsodae/menu/fixture/MenuCategoryMockFixture.java
@@ -13,33 +13,33 @@ import java.util.UUID;
 
 public class MenuCategoryMockFixture {
 
-  private MenuCategoryMockFixture() {
-    throw new AssertionError("Utility class should not be instantiated");
-  }
+    private MenuCategoryMockFixture() {
+        throw new AssertionError("Utility class should not be instantiated");
+    }
 
-  public static CategoryAndStoreFixture createCategoryAndStoreFixture(
-      MenuCategoryRepository menuCategoryRepository, UUID menuCategoryId, UUID storeId) {
-    MenuCategory category = mock(MenuCategory.class);
-    Store store = mock(Store.class);
-    given(menuCategoryRepository.findByIdAndDeletedIsFalse(menuCategoryId))
-        .willReturn(Optional.of(category));
-    lenient().when(category.getStore()).thenReturn(store);
-    lenient().when(store.getId()).thenReturn(storeId);
-    lenient().when(category.getId()).thenReturn(menuCategoryId);
-    lenient().when(category.getName()).thenReturn(DEFAULT_CATEGORY_NAME);
-    return new CategoryAndStoreFixture(category, store);
-  }
+    public static CategoryAndStoreFixture createCategoryAndStoreFixture(
+            MenuCategoryRepository menuCategoryRepository, UUID menuCategoryId, UUID storeId) {
+        MenuCategory category = mock(MenuCategory.class);
+        Store store = mock(Store.class);
+        given(menuCategoryRepository.findByIdAndDeletedIsFalse(menuCategoryId))
+                .willReturn(Optional.of(category));
+        lenient().when(category.getStore()).thenReturn(store);
+        lenient().when(store.getId()).thenReturn(storeId);
+        lenient().when(category.getId()).thenReturn(menuCategoryId);
+        lenient().when(category.getName()).thenReturn(DEFAULT_CATEGORY_NAME);
+        return new CategoryAndStoreFixture(category, store);
+    }
 
-  public static MenuCategory createMockCategory(UUID categoryId, String categoryName) {
-    MenuCategory category = mock(MenuCategory.class);
-    lenient().when(category.getId()).thenReturn(categoryId);
-    lenient().when(category.getName()).thenReturn(categoryName);
-    return category;
-  }
+    public static MenuCategory createMockCategory(UUID categoryId, String categoryName) {
+        MenuCategory category = mock(MenuCategory.class);
+        lenient().when(category.getId()).thenReturn(categoryId);
+        lenient().when(category.getName()).thenReturn(categoryName);
+        return category;
+    }
 
-  public static MenuCategory createMockCategory(UUID categoryId) {
-    return createMockCategory(categoryId, DEFAULT_CATEGORY_NAME);
-  }
+    public static MenuCategory createMockCategory(UUID categoryId) {
+        return createMockCategory(categoryId, DEFAULT_CATEGORY_NAME);
+    }
 
-  public record CategoryAndStoreFixture(MenuCategory category, Store store) {}
+    public record CategoryAndStoreFixture(MenuCategory category, Store store) {}
 }
--- src/test/java/com/project/baedalsodae/menu/fixture/MenuItemMockFixture.java
+++ src/test/java/com/project/baedalsodae/menu/fixture/MenuItemMockFixture.java
@@ -9,62 +9,43 @@ import com.project.baedalsodae.menu.entity.MenuItem;
 import com.project.baedalsodae.menu.entity.enums.MenuStatus;
 import java.util.UUID;
 
-
 public class MenuItemMockFixture {
 
-  private MenuItemMockFixture() {
-    throw new AssertionError("Utility class should not be instantiated");
-  }
+    private MenuItemMockFixture() {
+        throw new AssertionError("Utility class should not be instantiated");
+    }
 
+    public static MenuItem createFullyMockedMenuItem(
+            UUID id,
+            String name,
+            String description,
+            int price,
+            int orderNo,
+            boolean isPopular,
+            MenuStatus menuStatus,
+            MenuCategory category) {
+        MenuItem item = mock(MenuItem.class);
+        lenient().when(item.getId()).thenReturn(id);
+        lenient().when(item.getName()).thenReturn(name);
+        lenient().when(item.getDescription()).thenReturn(description);
+        lenient().when(item.getPrice()).thenReturn(price);
+        lenient().when(item.getOrderNo()).thenReturn(orderNo);
+        lenient().when(item.isPopular()).thenReturn(isPopular);
+        lenient().when(item.getMenuStatus()).thenReturn(menuStatus);
+        lenient().when(item.getMenuCategory()).thenReturn(category);
+        return item;
+    }
 
-  public static MenuItem createFullyMockedMenuItem(
-      UUID id,
-      String name,
-      String description,
-      int price,
-      int orderNo,
-      boolean isPopular,
-      MenuStatus menuStatus,
-      MenuCategory category) {
-    MenuItem item = mock(MenuItem.class);
-    lenient().when(item.getId()).thenReturn(id);
-    lenient().when(item.getName()).thenReturn(name);
-    lenient().when(item.getDescription()).thenReturn(description);
-    lenient().when(item.getPrice()).thenReturn(price);
-    lenient().when(item.getOrderNo()).thenReturn(orderNo);
-    lenient().when(item.isPopular()).thenReturn(isPopular);
-    lenient().when(item.getMenuStatus()).thenReturn(menuStatus);
-    lenient().when(item.getMenuCategory()).thenReturn(category);
-    return item;
-  }
-
-
-  public static MenuItem createMockMenuItemWithDefaultFields(
-      UUID menuItemId, MenuCategory category, int orderNo) {
-    MenuItem item = mock(MenuItem.class);
-    lenient().when(item.getId()).thenReturn(menuItemId);
-    lenient().when(item.getName()).thenReturn(DEFAULT_MENU_ITEM_NAME);
-    lenient().when(item.getDescription()).thenReturn(DEFAULT_ITEM_DESCRIPTION);
-    lenient().when(item.getPrice()).thenReturn(DEFAULT_ITEM_PRICE);
-    lenient().when(item.getOrderNo()).thenReturn(orderNo);
-    lenient().when(item.isPopular()).thenReturn(false);
-    lenient().when(item.getMenuStatus()).thenReturn(MenuStatus.AVAILABLE);
-    lenient().when(item.getMenuCategory()).thenReturn(category);
-    return item;
-  }
-
-
-  public static MenuItem createMockMenuItem(
-      UUID id,
-      String name,
-      String description,
-      int price,
-      int orderNo,
-      boolean isPopular,
-      MenuStatus menuStatus,
-      MenuCategory category) {
-    return createFullyMockedMenuItem(
-        id, name, description, price, orderNo, isPopular, menuStatus, category);
-  }
+    public static MenuItem createMockMenuItem(
+            UUID id,
+            String name,
+            String description,
+            int price,
+            int orderNo,
+            boolean isPopular,
+            MenuStatus menuStatus,
+            MenuCategory category) {
+        return createFullyMockedMenuItem(
+                id, name, description, price, orderNo, isPopular, menuStatus, category);
+    }
 }
-
