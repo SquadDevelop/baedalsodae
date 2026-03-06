78d7fce6cb3034915f84b2269747d38004a3c522
hyew0nn <hyewon-0607@naver.com>
2026-03-06 12:26:57 +0900

    feat: owner 위한 가게 상세 조회 DTO 추가

--- src/main/java/com/project/baedalsodae/store/dto/response/StoreResponse.java
+++ src/main/java/com/project/baedalsodae/store/dto/response/StoreResponse.java
@@ -1,26 +1,38 @@
 package com.project.baedalsodae.store.dto.response;
 
+import com.project.baedalsodae.global.common.dto.AddressResponse;
 import com.project.baedalsodae.store.entity.Store;
-import java.util.UUID;
+import com.project.baedalsodae.store.entity.enums.StoreStatus;
 import lombok.Builder;
 import lombok.Getter;
 
-@Builder
+import java.util.UUID;
+
 @Getter
+@Builder
 public class StoreResponse {
     private UUID storeId;
     private String storeName;
+    private String phoneNumber;
+    private String description;
     private Integer reviewCount;
     private Double avgRating;
     private String storeStatus;
+    private StoreStatus status;
+    private AddressResponse address;
+
+    // TODO: 가게 운영시간 추가
 
     public static StoreResponse fromEntity(Store store) {
         return StoreResponse.builder()
                 .storeId(store.getId())
                 .storeName(store.getName())
+                .phoneNumber(store.getPhone())
+                .description(store.getDescription())
                 .reviewCount(store.getReviewCount())
                 .avgRating(store.getAvgRating())
                 .storeStatus(store.getStoreStatus().name())
+                .address(AddressResponse.fromEntity(store.getAddress()))
                 .build();
     }
 }
