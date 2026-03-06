c74c9f982f6a6f14e9724193bf9d157ba4f9d10b
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-03-06 18:37:01 +0900

    fix: 공개 API에 요청 시 ìpermitAll JWT ìì토큰 검사 절차 생략

--- src/main/java/com/project/baedalsodae/auth/security/JwtAuthorizationFilter.java
+++ src/main/java/com/project/baedalsodae/auth/security/JwtAuthorizationFilter.java
@@ -31,6 +31,13 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     private final ObjectMapper objectMapper;
 
     @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getRequestURI();
+
+        return path.startsWith("/api/v1/auth");
+    }
+
+    @Override
     protected void doFilterInternal(
             HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
