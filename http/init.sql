fa2151fc49921edc0dacdf1ebdee967d98263ae4
jaebin1234 <jaebin2586@gmail.com>
2026-03-06 17:36:18 +0900

    feat: 주문 상태변경 관련  .http 추가

--- http/init.sql
+++ http/init.sql
@@ -17,11 +17,11 @@ INSERT INTO baedalsodae.p_store_category (is_deleted, created_at, deleted_at, up
 VALUES (DEFAULT, null, null, null, null, null, '00000000-0000-0000-0000-000000000001', null, '중식', null);
 
 -- 가게 추가
-INSERT INTO baedalsodae.p_store (is_deleted, rating_sum, review_count, created_at, deleted_at, updated_at,
+INSERT INTO baedalsodae.p_store (is_deleted, avg_rating, review_count, created_at, deleted_at, updated_at,
                                  business_number, created_by, deleted_by, id, store_category_id, updated_by, user_id,
                                  dong_code, phone, sido_code, sigungu_code, dong_name, name, sido_name, sigungu_name,
                                  description, detail_address, road_address, store_status)
-VALUES (DEFAULT, 0, 0, null, null, null, '1111111111', null, null, '00000000-0000-0000-0000-000000000001',
+VALUES (DEFAULT,  0, 0,null, null, null, '1111111111', null, null, '00000000-0000-0000-0000-000000000001',
         '00000000-0000-0000-0000-000000000001', null, '00000000-0000-0000-0000-000000000001', '1', '01011111111', '2',
         '3', '포일동', '가게1', '경기도', '안양시', null, '오금로', '16', 'OPEN');
 
@@ -92,6 +92,7 @@ INSERT INTO baedalsodae.p_menu_item (is_deleted, is_popular, order_no, price, cr
                                      name)
 VALUES (DEFAULT, true, 1, 18000, null, null, null, null, null, '00000000-0000-0000-0000-000000000003',
         '00000000-0000-0000-0000-000000000001', null, null, null, '치킨');
+
 --
 -- -- 장바구니 추가
 -- INSERT INTO baedalsodae.p_cart (created_at, updated_at, id, store_id, user_id)
