1c6724f1f87ed4f701c9143e6cb1c05a2f1de1af
annieha <hajh5447@naver.com>
2026-03-06 10:32:43 +0900

    feat: MenuCategory PartialIndex 적용

---
+++ src/main/java/com/project/baedalsodae/global/component/DatabaseIndexInitializer.java
@@ -0,0 +1,24 @@
+package com.project.baedalsodae.global.component;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DatabaseIndexInitializer implements ApplicationRunner {
+
+  private final JdbcTemplate jdbcTemplate;
+
+  @Override
+  public void run(ApplicationArguments args) {
+    jdbcTemplate.execute(
+        """
+      CREATE UNIQUE INDEX IF NOT EXISTS uq_menu_category_name_active
+      ON baedalsodae.p_menu_category (store_id, name)
+      WHERE is_deleted = false
+      """);
+  }
+}
