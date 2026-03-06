8a83041769e66a9f7063453b2236a09ad00e4af7
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-03-06 10:27:04 +0900

    feat: JWT 토큰 발급 기능 추가

---
+++ src/main/java/com/project/baedalsodae/auth/security/JwtProvider.java
@@ -0,0 +1,86 @@
+package com.project.baedalsodae.auth.security;
+
+import com.project.baedalsodae.global.common.ErrorCode;
+import com.project.baedalsodae.user.entity.UserRole;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecurityException;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class JwtProvider {
+
+	private static final String AUTHORIZATION_HEADER = "Authorization";
+	private static final String BEARER_PREFIX = "Bearer ";
+
+	private static final String USER_ID = "userId";
+	private static final String USER_ROLE = "userRole";
+	private static final String IS_DELETED = "isDeleted";
+
+	private static final long ACCESS_TOKEN_TIME = 30*60*1000L;
+
+	@Value("${JWT_SECRET_KEY}")
+	private String secretKey;
+	private SecretKey jwtSecretKey;
+
+	@PostConstruct
+	public void init() {
+		this.jwtSecretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+	}
+
+	public String createAccessToken(UUID id, String username, UserRole role, boolean isDeleted) {
+		Date now = new Date();
+		Date expiration = new Date(now.getTime() + ACCESS_TOKEN_TIME);
+		String newAccessToken = Jwts.builder()
+				.subject(username)
+				.claim(USER_ID, id.toString())
+				.claim(USER_ROLE, role.getRole())
+				.claim(IS_DELETED, isDeleted)
+				.expiration(expiration)
+				.issuedAt(now)
+				.signWith(jwtSecretKey, Jwts.SIG.HS256)
+				.compact();
+
+		return String.format("%s%s", BEARER_PREFIX, newAccessToken);
+	}
+
+	public String resolveToken(HttpServletRequest request) {
+		String token = request.getHeader(AUTHORIZATION_HEADER);
+
+		if (!StringUtils.hasText(token) || !token.startsWith(BEARER_PREFIX)) {
+			return null;
+		}
+
+		return token.substring(BEARER_PREFIX.length());
+	}
+
+	public Claims getClaims(String inputToken) {
+		try {
+			return Jwts.parser()
+					.verifyWith(jwtSecretKey)
+					.build()
+					.parseSignedClaims(inputToken)
+					.getPayload();
+		} catch (SecurityException | MalformedJwtException e) {
+			log.error(ErrorCode.JWT_SIGNATURE_INVALID.getMessage());
+		} catch (ExpiredJwtException e) {
+			log.error(ErrorCode.JWT_EXPIRED.getMessage());
+		} catch (UnsupportedJwtException e) {
+			log.error(ErrorCode.JWT_UNSUPPORTED.getMessage());
+		} catch (IllegalArgumentException e) {
+			log.error(ErrorCode.JWT_INVALID.getMessage());
+		}
+		return null;
+	}
+}
