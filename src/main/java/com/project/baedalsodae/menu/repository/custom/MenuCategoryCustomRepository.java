2eab93195e04040ec29cb710910e00a7fca29036
hyew0nn <hyewon-0607@naver.com>
2026-03-06 12:24:49 +0900

    feat: 가게 메뉴 아이템 목록을 조회하는 Repository 추가(QueryDSL)

---
+++ src/main/java/com/project/baedalsodae/menu/repository/custom/MenuCategoryCustomRepository.java
@@ -0,0 +1,10 @@
+package com.project.baedalsodae.menu.repository.custom;
+
+import com.project.baedalsodae.menu.dto.responseDto.category.MenuCategoryItemsResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface MenuCategoryCustomRepository {
+    List<MenuCategoryItemsResponse> getStoreCategoryItems(UUID storeId);
+}
