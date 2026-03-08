bc3e4b00b4ff56b1f0ce205d23566114d7ca61ec
hyew0nn <hyewon-0607@naver.com>
2026-03-09 01:36:10 +0900

    feat: SortType 검증 메소드 추가

--- src/main/java/com/project/baedalsodae/store/entity/enums/SortType.java
+++ src/main/java/com/project/baedalsodae/store/entity/enums/SortType.java
@@ -3,5 +3,9 @@ package com.project.baedalsodae.store.entity.enums;
 public enum SortType {
     LATEST,
     RATING,
-    REVIEW
+    REVIEW;
+
+    public boolean isStoreListSort() {
+        return this == LATEST || this == RATING || this == REVIEW;
+    }
 }
