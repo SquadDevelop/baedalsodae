ad43b3235221a0f93be92e21fd3ccd1b37fb15d8
annieha <hajh5447@naver.com>
2026-03-09 17:46:34 +0900

    fix: 중복되는 에러 에러코드 추가 후 통일
    # Conflicts:
    #	src/test/java/com/project/baedalsodae/store/fixture/StoreHoursRequestFixture.java

--- src/main/java/com/project/baedalsodae/global/common/ErrorCode.java
+++ src/main/java/com/project/baedalsodae/global/common/ErrorCode.java
@@ -18,6 +18,7 @@ public enum ErrorCode {
     JWT_UNSUPPORTED("Z008", HttpStatus.UNAUTHORIZED, "지원하지 않는 JWT 토큰입니다."),
     LOGIN_FAILED("Z009", HttpStatus.UNAUTHORIZED, "로그인 요청 처리에 실패했습니다."),
     SORT_UNSUPPORTED("Z010", HttpStatus.UNAUTHORIZED, "지원하지 않는 정렬입니다."),
+    UTILITY_ASSERTION("Z011", HttpStatus.INTERNAL_SERVER_ERROR, "유틸리티 클래스의 생성자 호출은 허용되지 않습니다."),
 
     // database
     DATABASE_UNAVAILABLE("DA001", HttpStatus.SERVICE_UNAVAILABLE, "데이터베이스 연결이 불가능합니다"),
--- src/test/java/com/project/baedalsodae/store/fixture/StoreHoursRequestFixture.java
+++ src/test/java/com/project/baedalsodae/store/fixture/StoreHoursRequestFixture.java
@@ -2,7 +2,9 @@ package com.project.baedalsodae.store.fixture;
 
 import static com.project.baedalsodae.store.fixture.StoreHoursTestConstants.*;
 
-import com.project.baedalsodae.store.dto.request.store.StoreHoursRequest;
+import com.project.baedalsodae.global.common.BusinessException;
+import com.project.baedalsodae.global.common.ErrorCode;
+import com.project.baedalsodae.store.dto.request.StoreHoursRequest;
 import com.project.baedalsodae.store.entity.enums.DayOfWeek;
 import java.util.Arrays;
 import java.util.List;
@@ -10,7 +12,7 @@ import java.util.List;
 public class StoreHoursRequestFixture {
 
     private StoreHoursRequestFixture() {
-        throw new AssertionError("Utility class should not be instantiated");
+        throw new BusinessException(ErrorCode.UTILITY_ASSERTION);
     }
 
     public static List<StoreHoursRequest> createDefaultRequests() {
