38eedd7f188b81e5609878497ab8656dfcf64edb
jaebin1234 <jaebin2586@gmail.com>
2026-03-11 22:00:10 +0900

    refactor: spotless

--- src/main/java/com/project/baedalsodae/global/component/DatabaseIndexInitializer.java
+++ src/main/java/com/project/baedalsodae/global/component/DatabaseIndexInitializer.java
@@ -3,7 +3,6 @@ package com.project.baedalsodae.global.component;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.core.JdbcTemplate;
